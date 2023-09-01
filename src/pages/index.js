e995e9763bbd3094f7928d85869d1ebd27cb2a74
twoghoststrategy <143116164+twoghoststrategy@users.noreply.github.com>
2023-09-01 22:05:15 +0000

    test and case

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ import PartnerLogos from "../components/PartnerLogos"
 import News from "../components/PostCard/feed"
 import Services from "../components/Services/Services"
 import Model from "../components/Strategy/model"
-import Clients from '../components/Clients/composer'
+// import Clients from '../components/Clients/composer'
 const IndexPage = ({ location }) => (
     <>
 
@@ -33,7 +33,7 @@ const IndexPage = ({ location }) => (
 
             <section id="About" className="w-full px-10 py-10 xl:px-10 lg:px-10 md:px-10"><About /></section>
             <section id="Services" className="w-full px-10 py-10 xl:px-10 lg:px-10 md:px-10"><Services /></section>
-            <section id="Clients" className="w-full px-10 py-10 xl:px-10 lg:px-10 md:px-10"><Clients /></section>
+            {/* <section id="Clients" className="w-full px-10 py-10 xl:px-10 lg:px-10 md:px-10"><Clients /></section> */}
             <section id="Blogs" className="w-full px-10 py-10 mx-10"><News /></section>
 
         </Layout>
